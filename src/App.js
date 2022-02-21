c7a54fcb9941b405ffa91432f83aaa76a2d1db41
Shahid Bahadur <shahidbahadur1998@gmail.com>
2022-02-21 17:09:39 +0500

    Raect Timer updating every second

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+
+const App = () => {
+  let time = new Date()
+  const [timer, setTimer] = useState(time.toLocaleTimeString())
+
+  const tick = () => {
+    setTimer(time.toLocaleTimeString())
+  }
+
+  useEffect(() => {
+    const interval = setInterval(tick, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [timer])
+  return (
+    <div className='flex justify-center items-center font-bold text-5xl'>
+      {timer}
+    </div>
+  )
+}
+
+export default App
